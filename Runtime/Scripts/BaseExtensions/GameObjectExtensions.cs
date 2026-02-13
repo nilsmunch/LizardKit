7b2cc8057db948e3358f0bc67b85f71ccc4e30d8
Nils Munch <gecko@wolflord.dev>
2026-02-13 16:52:07 +0100

    Gameobject cleanup

---
+++ Runtime/Scripts/BaseExtensions/GameObjectExtensions.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace LizardKit.BaseExtensions
+{
+    public static class GameObjectExtensions
+    {
+        public static void ClearChildren(this GameObject go, bool immediate = false)
+        {
+            if (!go) return;
+            
+            go.transform.ClearChildren(immediate);
+        }
+
+        public static void ClearChildren(this Transform transform, bool immediate = false)
+        {
+            if (!transform) return;
+
+            for (var i = transform.childCount - 1; i >= 0; i--)
+            {
+                var child = transform.GetChild(i);
+
+#if UNITY_EDITOR
+                if (!Application.isPlaying && immediate)
+                    Object.DestroyImmediate(child.gameObject);
+                else
+#endif
+                    Object.Destroy(child.gameObject);
+            }
+        }
+    }
+}
