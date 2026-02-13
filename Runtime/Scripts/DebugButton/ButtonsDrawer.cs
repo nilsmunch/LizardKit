b38ab9d0bd60576175a22389340d89ef29087efd
Nils Munch <gecko@wolflord.dev>
2026-02-13 14:56:49 +0100

    Debug button

---
+++ Runtime/Scripts/DebugButton/ButtonsDrawer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+
+#if UNITY_EDITOR
+namespace LizardKit.DebugButton.Editor
+{
+    public class ButtonsDrawer
+    {
+        private readonly List<IGrouping<string, DebugButton>> _buttonGroups;
+
+        public ButtonsDrawer(object target)
+        {
+            const BindingFlags flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            var methods = target.GetType().GetMethods(flags);
+            var buttons = new List<DebugButton>();
+            var rowNumber = 0;
+
+            foreach (MethodInfo method in methods)
+            {
+                var buttonAttribute = method.GetCustomAttribute<ButtonAttribute>();
+
+                if (buttonAttribute == null)
+                    continue;
+
+                buttons.Add(new DebugButton(method, buttonAttribute));
+            }
+
+            _buttonGroups = buttons.GroupBy(button =>
+                {
+                    var attribute = button.ButtonAttribute;
+                    var hasRow = attribute.HasRow;
+                    return hasRow ? attribute.Row : $"__{rowNumber++}";
+                }).ToList();
+        }
+
+        public void DrawButtons(IEnumerable<object> targets)
+        {
+            foreach (var buttonGroup in _buttonGroups)
+            {
+                if(buttonGroup.Any())
+                {
+                    var space = buttonGroup.First().ButtonAttribute.Space;
+                    if(space != 0) EditorGUILayout.Space(space);
+                }
+                using (new EditorGUILayout.HorizontalScope())
+                {
+                    foreach (var button in buttonGroup)
+                    {
+                        button.Draw(targets);
+                    }
+                }
+            }
+        }
+    }
+}
+
+#endif
---
+++ Runtime/Scripts/DebugButton/DebugButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+namespace LizardKit.DebugButton.Editor
+{
+    public class DebugButton
+    {
+        private readonly string _displayName;
+        private readonly MethodInfo _method;
+        public readonly ButtonAttribute ButtonAttribute;
+
+        public DebugButton(MethodInfo method, ButtonAttribute buttonAttribute)
+        {
+            ButtonAttribute = buttonAttribute;
+            _displayName = string.IsNullOrEmpty(buttonAttribute.Name)
+                ? ObjectNames.NicifyVariableName(method.Name)
+                : buttonAttribute.Name;
+
+            _method = method;
+        }
+
+        internal void Draw(IEnumerable<object> targets)
+        {
+            if (!GUILayout.Button(_displayName)) return;
+
+            foreach (object target in targets)
+            {
+                _method.Invoke(target, null);
+            }
+        }
+    }
+}
+#endif
