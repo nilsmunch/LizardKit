4e71f0ef9b92161006ead6046d3c0f34a65f74b7
Nils Munch <gecko@wolflord.dev>
2026-02-13 16:46:05 +0100

    Class Collection catalog

---
+++ Runtime/Scripts/Scaffolding/ClassCollectionUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LizardKit.Scaffolding
+{
+    public class ClassCollectionUtil
+    {
+        private static List<Type> GetAllConcreteSubclassesOf<TBase>()
+        {
+            var baseType = typeof(TBase);
+
+            return AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => 
+                    baseType.IsAssignableFrom(t) &&
+                    t != baseType &&
+                    !t.IsAbstract)
+                .ToList();
+        }
+    }
+}
