1c2770f193951340e2da24e7980f84f14a344554
Nils Munch <gecko@wolflord.dev>
2026-02-19 11:26:11 +0100

    ObjectEditor only fallback

--- Editor/DebugButton/ObjectEditor.cs
+++ Editor/DebugButton/ObjectEditor.cs
@@ -10,7 +10,7 @@ using Object = UnityEngine.Object;
 #if UNITY_EDITOR
 namespace LizardKit.DebugButton
 {
-    [CustomEditor(typeof(Object), true)]
+    [CustomEditor(typeof(Object), true, isFallback = true)]
     internal class ObjectEditor : UnityEditor.Editor
     {
         private ButtonsDrawer _buttonsDrawer;
