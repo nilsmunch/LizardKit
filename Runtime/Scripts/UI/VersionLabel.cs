f18fd6ba8517022b29a261f05aebcbd2bf49cc4f
Nils Munch <gecko@wolflord.dev>
2026-03-31 08:33:20 +0200

    Version label

---
+++ Runtime/Scripts/UI/VersionLabel.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace LizardKit.UI
+{
+    public class VersionLabel : MonoBehaviour
+    {
+        private void Awake()
+        {
+            var label =  GetComponent<TMP_Text>();
+            label.text = "v." + Application.version;
+        }
+    }
+}
