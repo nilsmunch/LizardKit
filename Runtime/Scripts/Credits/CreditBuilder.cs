9c0571ba7855971c34369c07c7dcd44b2e221c9b
Nils Munch <gecko@wolflord.dev>
2026-05-02 23:11:51 +0200

    Credits builder

--- Runtime/Scripts/Credits/CreditBuilder.cs
+++ Runtime/Scripts/Credits/CreditBuilder.cs
@@ -1,55 +1,136 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
+using System.Text;
 using LizardKit.DebugButton;
 using TMPro;
 using UnityEngine;
 
 namespace LizardKit.Credits
 {
+    public enum CreditLayoutStyle
+    {
+        TwoColumn, // Age of Empires style
+        CenterStacked // Alba style
+    }
+
     public abstract class CreditBuilder : MonoBehaviour
     {
-        public TMP_Text titleLabel;
-        public TMP_Text namesLabel;
+        [Header("References")] public TMP_Text creditsLabel;
         public RectTransform scrollSpace;
+
+        [Header("Layout")] public CreditLayoutStyle layoutStyle = CreditLayoutStyle.TwoColumn;
+
+        [Header("Text Styling")] public string sectionTitleColor = "#D6A84A";
+        public string roleColor = "#FFFFFF";
+        public string nameColor = "#FFFFFF";
+
+        public int sectionTitleSize = 32;
+        public int roleSize = 24;
+        public int nameSize = 26;
+
+        [Header("Two Column")] public float roleColumnWidth = 520f;
+        public float nameColumnWidth = 520f;
+
+        [Header("Spacing")] public int linesAfterEntry = 1;
+        public int linesAfterSectionTitle = 1;
 
         protected virtual List<CreditEntry> GetCredits()
         {
-            var credits = new List<CreditEntry>();
-            return credits;
+            return new List<CreditEntry>();
+        }
+
+        private void Awake()
+        {
+            BuildCredits();
         }
 
         private void OnEnable()
         {
+            BuildCredits();
             StartCoroutine(BuildRect());
         }
 
         private IEnumerator BuildRect()
         {
             yield return new WaitForEndOfFrame();
-            var displayHeight = namesLabel.preferredHeight + 70;
-            if (scrollSpace) scrollSpace.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, displayHeight);
-        }
-        private void Awake()
-        {
-            BuildCredits();
+
+            if (!creditsLabel || !scrollSpace)
+                yield break;
+
+            var displayHeight = creditsLabel.preferredHeight + 70;
+            scrollSpace.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, displayHeight);
         }
 
         [Button]
         protected virtual void BuildCredits()
         {
-            var creditString = "";
-            foreach (var credit in GetCredits())
+            if (!creditsLabel)
+                return;
+
+            creditsLabel.richText = true;
+
+            var credits = GetCredits();
+            creditsLabel.text = layoutStyle switch
             {
-                creditString += $"{credit.Title}\n";
-                for (var i = 1; i < credit.Count; i++)
-                {
-                    creditString += "\n";
-                }
+                CreditLayoutStyle.TwoColumn => BuildTwoColumnCredits(credits),
+                CreditLayoutStyle.CenterStacked => BuildCenterStackedCredits(credits),
+                _ => BuildTwoColumnCredits(credits)
+            };
+        }
+
+        private string BuildTwoColumnCredits(List<CreditEntry> credits)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("<align=center>");
+            sb.AppendLine($"<color={sectionTitleColor}><size={sectionTitleSize}>CREDITS</size></color>");
+            AppendBlankLines(sb, linesAfterSectionTitle);
+            sb.AppendLine("</align>");
+
+            foreach (var credit in credits)
+            {
+                sb.Append("<align=center>");
+
+                sb.Append($"<size={roleSize}><color={roleColor}>");
+                sb.Append($"<pos=0%><width={roleColumnWidth}>{credit.Title}</width>");
+                sb.Append("</color></size>");
+
+                sb.Append($"<size={nameSize}><color={nameColor}>");
+                sb.Append($"<pos=52%><width={nameColumnWidth}>{credit.Name}</width>");
+                sb.Append("</color></size>");
+
+                sb.AppendLine("</align>");
+
+                AppendBlankLines(sb, Mathf.Max(credit.Count - 1, linesAfterEntry));
             }
-            titleLabel.text = creditString;
-            var namesString = GetCredits().Aggregate("", (current, credit) => current + $"{credit.Name}\n");
-            namesLabel.text = namesString.Trim();
+
+            return sb.ToString().Trim();
+        }
+
+        private string BuildCenterStackedCredits(List<CreditEntry> credits)
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine("<align=center>");
+
+            foreach (var credit in credits)
+            {
+                if (string.IsNullOrEmpty(credit.Name.Trim())) continue;
+                sb.AppendLine($"<size={roleSize}><color={roleColor}>{credit.Title}</color></size>");
+                sb.AppendLine($"<size={nameSize}><color={nameColor}><b>{credit.Name}</b></color></size>");
+
+                AppendBlankLines(sb, 1);
+            }
+
+            sb.AppendLine("</align>");
+
+            return sb.ToString().Trim();
+        }
+
+        private static void AppendBlankLines(StringBuilder sb, int count)
+        {
+            for (var i = 0; i < count; i++)
+                sb.AppendLine();
         }
     }
 }
