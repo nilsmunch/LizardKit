5f2574c4111fafd872c74273f677b183c952ea34
Nils Munch <gecko@wolflord.dev>
2026-05-02 23:06:52 +0200

    Patreon Credits Loader

---
+++ Runtime/Scripts/Credits/PatreonCreditsLoader.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace MetaMorph.CoreGame.Credits
+{
+    public static class PatreonCreditsLoader
+    {
+        private const string Url = "https://connect.lewdlizard.com/patreon/api/credits.json";
+        public static List<string> PatreonSupporters = new();
+        public static List<string> PatreonVipSupporters = new();
+        
+        public static IEnumerator Load()
+        {
+            using var request = UnityWebRequest.Get(Url);
+            yield return request.SendWebRequest();
+
+            if (request.result != UnityWebRequest.Result.Success)
+            {
+                Debug.LogError("Failed to load Patreon credits: " + request.error);
+                yield break;
+            }
+
+            var json = request.downloadHandler.text;
+            var data = JsonUtility.FromJson<PatreonCreditsResponse>(json);
+
+            PatreonSupporters = data.normals ?? new List<string>();
+            PatreonVipSupporters = data.vips ?? new List<string>();
+        }
+    
+        [System.Serializable]
+        public class PatreonCreditsResponse
+        {
+            public List<string> normals;
+            public List<string> vips;
+        }
+    }
+}
