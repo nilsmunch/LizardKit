07a1ea10fd0454b43ab69a37038afb5208b71125
Nils Munch <gecko@wolflord.dev>
2026-06-21 13:38:54 +0200

    One of each class utility

---
+++ Runtime/Scripts/Scaffolding/TypeCatalogs/OneOfEach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LizardKit.Scaffolding.TypeCatalogs
+{
+    public class OneOfEach<T> : List<T> where T : class
+    {
+        public OneOfEach()
+        {
+            AddRange(
+                AppDomain.CurrentDomain.GetAssemblies()
+                    .SelectMany(a => a.GetTypes())
+                    .Where(type =>
+                        typeof(T).IsAssignableFrom(type) &&
+                        !type.IsAbstract &&
+                        !type.IsInterface &&
+                        type.GetConstructor(Type.EmptyTypes) != null)
+                    .Select(type => (T)Activator.CreateInstance(type))
+            );
+        }
+    }
+}
