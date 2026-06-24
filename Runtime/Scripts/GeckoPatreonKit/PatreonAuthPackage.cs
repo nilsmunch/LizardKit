54b359b84ecb9b4b13f8ee341bd16997dc7d64e6
Nils Munch <gecko@wolflord.dev>
2026-06-24 15:30:11 +0200

    Auth pack text areas

--- Runtime/Scripts/GeckoPatreonKit/PatreonAuthPackage.cs
+++ Runtime/Scripts/GeckoPatreonKit/PatreonAuthPackage.cs
@@ -10,6 +10,7 @@ public class PatreonAuthPackage : ScriptableObject
         public string gameSlug;
         public List<string> patreonFeatures;
         public string safetyKey;
+        [TextArea(5,30)]
         public string unlockBenefitDescription = "This unlocks bonus content across all our games.";
         public string freeUserBenefits = "It seems you are not currently supporting the LizardFactory team, so there are no benefits.";
         public string paidUserBenefits = "Your bonus content has been unlocked.";
