5e876e0b8fe23598f00691896a55286467b19b2e
Nils Munch <gecko@wolflord.dev>
2026-06-26 14:14:04 +0200

    Main menu panel

---
+++ Runtime/Scripts/UI/BaseMainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using LizardKit.Settings;
+using UnityEngine;
+
+namespace LizardKit.UI
+{
+    public class BaseMainMenu : MonoBehaviour
+    {
+        private List<BaseMainMenuPanel> _menuPages;
+
+        protected virtual void Awake()
+        {
+            _menuPages = FindObjectsByType<BaseMainMenuPanel>(FindObjectsInactive.Include, FindObjectsSortMode.None).ToList();
+            MasterVolumeSlider.GlobalPreload();
+        }
+
+        public virtual void ShiftPage(string pageRequest)
+        {
+            foreach (var page in _menuPages)
+            {
+                page.gameObject.SetActive(page.key == pageRequest);
+            }
+        }
+        
+        public void QuitToDesktop()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Runtime/Scripts/UI/BaseMainMenuPanel.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace LizardKit.UI
+{
+    public class BaseMainMenuPanel : MonoBehaviour
+    {
+        public string key;
+    }
+}
